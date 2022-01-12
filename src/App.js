ab47fbf20a7ed311a337f5cb146bbcb4c4841d36
Hanfu-Zhang <zhf991102@163.com>
2022-01-12 16:53:10 +0800

    引入antd和两个样式库

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Home from './components/Home'
+import { Layout, Menu, Breadcrumb } from 'antd';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const { Header, Content, Footer } = Layout;
+
+export default class App extends Component {
+  render() {
+    return (
+        <div>
+            <Layout className="layout">
+                <Header id="components-layout-demo-top">
+                    <div className="logo" />
+                    <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+                        {new Array(3).fill(null).map((_, index) => {
+                            const key = index + 1;
+                            return <Menu.Item key={key}>{`nav ${key}`}</Menu.Item>;
+                        })}
+                    </Menu>
+                </Header>
+                <Content style={{ padding: '0 50px' }}>
+                    <Breadcrumb style={{ margin: '16px 0' }}>
+                        <Breadcrumb.Item>Home</Breadcrumb.Item>
+                        <Breadcrumb.Item>List</Breadcrumb.Item>
+                        <Breadcrumb.Item>App</Breadcrumb.Item>
+                    </Breadcrumb>
+                    <div className="site-layout-content">
+                        <Home/>
+                    </div>
+                </Content>
+                <Footer style={{ textAlign: 'center' }}>PAI Fabric Visualization ©2022 Developed by Hanfu Zhang, Zongtai Li, and Shaoyi Han</Footer>
+            </Layout>
+        </div>
+    );
+  }
 }
 
-export default App;
+
+
